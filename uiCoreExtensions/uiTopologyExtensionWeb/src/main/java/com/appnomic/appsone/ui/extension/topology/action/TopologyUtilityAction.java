49f79abd09c42c70630f6f12045630f6190cd042
bharadwaj <bharath12345@gmail.com>
2013-05-26 13:13:14 +0530

    config ejb and topology for links

---
+++ uiCoreExtensions/uiTopologyExtensionWeb/src/main/java/com/appnomic/appsone/ui/extension/topology/action/TopologyUtilityAction.java
@@ -0,0 +1,57 @@
+package com.appnomic.appsone.ui.extension.topology.action;
+
+import com.appnomic.appsone.common.AbstractAction;
+import com.appnomic.appsone.config.entity.LinksListEntity;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+
+import java.util.Map;
+
+@ParentPackage("json-default")
+@Namespace("/topology")
+public class TopologyUtilityAction extends AbstractAction {
+
+	private LinksListEntity.LinkEntity[] linkEntityList;
+	private Map<String, String[]> param;
+	
+	public LinksListEntity.LinkEntity[] getLinkEntityList() {
+		return linkEntityList;
+	}
+
+	public void setLinkEntityList(LinksListEntity.LinkEntity[] linkEntityList) {
+		this.linkEntityList = linkEntityList;
+	}
+
+	public Map<String, String[]> getParam() {
+		return param;
+	}
+
+	public void setParam(Map<String, String[]> param) {
+		this.param = param;
+	}
+
+	@Action(value="links", results = {
+	        @Result(name="success", type="json", params = {
+	        		"excludeProperties",
+	                "parameters,session,SUCCESS,ERROR,agcVO,levelDbMap",
+	        		"enableGZIP", "true",
+	        		"encoding", "UTF-8",
+	                "noCache","true",
+	                "excludeNullProperties","true"
+	            })})
+	public String pagesAction() {
+		param = getParameters();
+
+        /*LinksListEntity nocPLE = UtilityAction.getPageListEntity(userUuid);
+        linkEntityList = nocPLE.getLinkEntity();*/
+
+        // ToDo: The below code is temporary - remove it once the persistence stuff starts working
+        linkEntityList = LinksListEntity.getDefaultTopologyPageEntity().getLinkEntity();
+
+
+        return SUCCESS;
+	}
+	
+}
